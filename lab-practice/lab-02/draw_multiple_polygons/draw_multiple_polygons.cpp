5f8aa207eb58ce3fec20d8b7242852caee8a5e35
ShahriarTbc <ripon23105101019@diu.edu.bd>
2026-01-22 21:06:27 +0600

    lab-2 update

--- lab-practice/lab-02/draw_multiple_polygons/draw_multiple_polygons.cpp
+++ lab-practice/lab-02/draw_multiple_polygons/draw_multiple_polygons.cpp
@@ -1,88 +1,92 @@
 #include <GL/glut.h>
+
 void display()
 {
     glClear(GL_COLOR_BUFFER_BIT);
 
-    /* ===== Left Star (Red) ===== */
     glColor3f(1.0, 0.0, 0.0);
+    glPointSize(5);
     glBegin(GL_POLYGON);
-    glVertex2f(-0.6,  0.0);
-    glVertex2f(-0.6,  0.6);
-    glVertex2f(-0.35, 0.15);
-    glVertex2f( 0.0,  0.0);
-    glVertex2f(-0.35,-0.15);
-    glVertex2f(-0.6, -0.6);
-    glVertex2f(-0.85,-0.15);
-    glVertex2f(-1.2,  0.0);
-    glVertex2f(-0.85, 0.15);
-    glVertex2f(-0.6,  0.6);
+        glVertex2f(0,0);
+        glVertex2f(0,8);
+        glVertex2f(2,2);
+        glVertex2f(8,0);
+        glVertex2f(2,-2);
+        glVertex2f(0,-8);
+        glVertex2f(-2,-2);
+        glVertex2f(-8,0);
+        glVertex2f(-2,2);
+        glVertex2f(0,8);
     glEnd();
 
-    /* ===== Top Star (Green) ===== */
     glColor3f(0.0, 1.0, 0.0);
     glBegin(GL_POLYGON);
-    glVertex2f(0.0,  0.6);
-    glVertex2f(0.0,  1.2);
-    glVertex2f(0.25, 0.75);
-    glVertex2f(0.6,  0.6);
-    glVertex2f(0.25, 0.45);
-    glVertex2f(0.0,  0.0);
-    glVertex2f(-0.25,0.45);
-    glVertex2f(-0.6, 0.6);
-    glVertex2f(-0.25,0.75);
-    glVertex2f(0.0,  1.2);
+        glVertex2f(8,8);
+        glVertex2f(8,16);
+        glVertex2f(10,10);
+        glVertex2f(16,8);
+        glVertex2f(10,6);
+        glVertex2f(8,0);
+        glVertex2f(6,6);
+        glVertex2f(0,8);
+        glVertex2f(6,10);
+        glVertex2f(8,16);
     glEnd();
 
-    /* ===== Right Star (White) ===== */
+
     glColor3f(1.0, 1.0, 1.0);
     glBegin(GL_POLYGON);
-    glVertex2f(0.6,  0.0);
-    glVertex2f(0.6,  0.6);
-    glVertex2f(0.85, 0.15);
-    glVertex2f(1.2,  0.0);
-    glVertex2f(0.85,-0.15);
-    glVertex2f(0.6, -0.6);
-    glVertex2f(0.35,-0.15);
-    glVertex2f(0.0,  0.0);
-    glVertex2f(0.35, 0.15);
-    glVertex2f(0.6,  0.6);
+        glVertex2f(16,0);
+        glVertex2f(16,8);
+        glVertex2f(18,2);
+        glVertex2f(24,0);
+        glVertex2f(18,-2);
+        glVertex2f(16,-8);
+        glVertex2f(14,-2);
+        glVertex2f(8,0);
+        glVertex2f(14,2);
+        glVertex2f(16,8);
     glEnd();
 
-    /* ===== Bottom Star (Blue) ===== */
+
     glColor3f(0.0, 0.0, 1.0);
     glBegin(GL_POLYGON);
-    glVertex2f(0.0, -0.6);
-    glVertex2f(0.0,  0.0);
-    glVertex2f(0.25,-0.45);
-    glVertex2f(0.6, -0.6);
-    glVertex2f(0.25,-0.75);
-    glVertex2f(0.0, -1.2);
-    glVertex2f(-0.25,-0.75);
-    glVertex2f(-0.6, -0.6);
-    glVertex2f(-0.25,-0.45);
-    glVertex2f(0.0,  0.0);
+        glVertex2f(8,-8);
+        glVertex2f(8,0);
+        glVertex2f(10,-6);
+        glVertex2f(16,-8);
+        glVertex2f(10,-10);
+        glVertex2f(8,-16);
+        glVertex2f(6,-10);
+        glVertex2f(0,-8);
+        glVertex2f(6,-6);
+        glVertex2f(8,0);
     glEnd();
 
     glFlush();
 }
 
+
 void init()
 {
-
-    glClearColor(0.6, 0.8, 1.0,1.0);
+    glClearColor(0.6, 0.8, 1.0, 1.0);
     glMatrixMode(GL_PROJECTION);
-    gluOrtho2D(-1.0, 1.0, -1.0, 1.0);
+    gluOrtho2D(-100.0, 100.0,
+               -100.0, 100.0);
 }
+
+
 int main(int argc, char** argv)
 {
-
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-    glutInitWindowSize(500,300);
+    glutInitWindowSize(800, 500);
     glutInitWindowPosition(100, 150);
-    glutCreateWindow("OpenGL Small Point");
+    glutCreateWindow("OpenGL POLYGON Project");
+
     init();
     glutDisplayFunc(display);
     glutMainLoop();
+
     return 0;
 }
