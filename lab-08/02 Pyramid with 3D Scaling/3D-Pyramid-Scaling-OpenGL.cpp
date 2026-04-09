5d2c2fad5635c69875f746aaf9fc3048751a1339
Shariar Ahamed <shahriar64.diu@gmail.com>
2026-04-09 14:40:19 +0600

    Implement 3D pyramid scaling with OpenGL
    This program creates a 3D pyramid using OpenGL and allows for scaling along the X, Y, and Z axes through keyboard input. It includes functions for drawing the pyramid and handling user input for scaling factors.

--- lab-08/02 Pyramid with 3D Scaling/3D-Pyramid-Scaling-OpenGL.cpp
+++ lab-08/02 Pyramid with 3D Scaling/3D-Pyramid-Scaling-OpenGL.cpp
@@ -1,1 +1,136 @@
+#include <GL/glut.h>
+#include <iostream>
+using namespace std;
 
+// Base square points
+float X1, Y1, Z1;
+float X2, Y2, Z2;
+
+// Top point
+float TXp, TYp, TZp;
+
+// Scaling factors
+float Sx, Sy, Sz;
+
+// Current scale
+float scaleX = 1, scaleY = 1, scaleZ = 1;
+
+// ================= PYRAMID =================
+void drawPyramidFromPoints(float x1, float y1, float z1,
+                           float x2, float y2, float z2,
+                           float txp, float typ, float tzp)
+{
+    // Bottom square
+    glBegin(GL_QUADS);
+
+    glColor3f(1, 1, 0);
+    glVertex3f(x1, y1, z1);
+    glVertex3f(x2, y1, z1);
+    glVertex3f(x2, y1, z2);
+    glVertex3f(x1, y1, z2);
+
+    glEnd();
+
+    // 4 triangle faces
+    glBegin(GL_TRIANGLES);
+
+    // Front
+    glColor3f(1, 0, 0);
+    glVertex3f(x1, y1, z1);
+    glVertex3f(x2, y1, z1);
+    glVertex3f(txp, typ, tzp);
+
+    // Right
+    glColor3f(0, 1, 0);
+    glVertex3f(x2, y1, z1);
+    glVertex3f(x2, y1, z2);
+    glVertex3f(txp, typ, tzp);
+
+    // Back
+    glColor3f(0, 0, 1);
+    glVertex3f(x2, y1, z2);
+    glVertex3f(x1, y1, z2);
+    glVertex3f(txp, typ, tzp);
+
+    // Left
+    glColor3f(1, 0, 1);
+    glVertex3f(x1, y1, z2);
+    glVertex3f(x1, y1, z1);
+    glVertex3f(txp, typ, tzp);
+
+    glEnd();
+}
+
+// ================= DISPLAY =================
+void display()
+{
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glLoadIdentity();
+
+    gluLookAt(15, 15, 25, 0, 0, 0, 0, 1, 0);
+
+    // Apply scaling
+    glScalef(scaleX, scaleY, scaleZ);
+
+    drawPyramidFromPoints(X1, Y1, Z1, X2, Y2, Z2, TXp, TYp, TZp);
+
+    glutSwapBuffers();
+}
+
+// ================= KEYBOARD =================
+void keyboard(unsigned char key, int x, int y)
+{
+    switch (key)
+    {
+    case 'X': scaleX += Sx; break;
+    case 'x': scaleX -= Sx; break;
+
+    case 'Y': scaleY += Sy; break;
+    case 'y': scaleY -= Sy; break;
+
+    case 'Z': scaleZ += Sz; break;
+    case 'z': scaleZ -= Sz; break;
+    }
+
+    glutPostRedisplay();
+}
+
+// ================= INIT =================
+void init()
+{
+    glEnable(GL_DEPTH_TEST);
+
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluPerspective(60, 1.0, 1.0, 100.0);
+
+    glMatrixMode(GL_MODELVIEW);
+}
+
+// ================= MAIN =================
+int main(int argc, char** argv)
+{
+    cout << "Enter base first point (x1 y1 z1): ";
+    cin >> X1 >> Y1 >> Z1;
+
+    cout << "Enter base opposite point (x2 y2 z2): ";
+    cin >> X2 >> Y2 >> Z2;
+
+    cout << "Enter top point (x y z): ";
+    cin >> TXp >> TYp >> TZp;
+
+    cout << "Enter scaling unit (Sx Sy Sz): ";
+    cin >> Sx >> Sy >> Sz;
+
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+    glutInitWindowSize(600, 600);
+    glutCreateWindow("3D Pyramid Scaling");
+
+    init();
+    glutDisplayFunc(display);
+    glutKeyboardFunc(keyboard);
+
+    glutMainLoop();
+    return 0;
+}
